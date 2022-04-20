d6502ec0b24cb5ddaa72f156ee8231d768972e5e
Tibsen <91956517+Tibsen@users.noreply.github.com>
2022-04-20 15:25:14 +0200

    Add files via upload

---
+++ Pim/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Route, Router } from "react-router-dom";
+import { Container } from "@mui/material";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+    return (
+        <Container>
+            <div className="App">
+                <Router>
+                    <Route path="/login" component={LoginPage}></Route>
+                </Router>
+            </div>
+        </Container>
+    );
+}
+
+export default App;
