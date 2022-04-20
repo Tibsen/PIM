92e194a5343b4b874435626c070cd206f1bdb6ce
Tibsen <91956517+Tibsen@users.noreply.github.com>
2022-04-20 16:48:20 +0200

    Add files via upload

---
+++ Pim/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Route, Router } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+    return (
+        <div>
+            <div className="App">
+                test
+            </div>
+        </div>
+        
+    );
+}
+
+export default App;
