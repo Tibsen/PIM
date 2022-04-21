ce37a9a0024a9b8f7e245311395a8323c8505cf5
Tommy Weng <senes@MacBook-Pro-de-Tommy.local>
2022-04-21 10:55:22 +0200

    buton

--- Pim/src/pages/LoginPage.jsx
+++ Pim/src/pages/LoginPage.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TextField} from "@mui/material";
+import { Button } from '@mui/material';
 
 const LoginPage = () => {
+    const [credentials, setCrentials] = useState({
+        identifier:"",
+        password:""
+    });
+
+    const handleChange =({currentTarget}) => {
+        console.log(currentTarget)
+        const value = currentTarget.value
+        const name = currentTarget.name
+        console.log(value,name)
+    }
     return (
         <form>
             <div>
@@ -10,6 +22,7 @@ const LoginPage = () => {
                 label="Username"
                 type="text"
                 name="identifiant"
+                onChange={handleChange}
                 />
             </div>
             <div>
@@ -18,7 +31,13 @@ const LoginPage = () => {
                 label="Password"
                 type="text"
                 name="password"
+                onChange={handleChange}
                 />
+            </div>
+            <div>
+                <Button variant="contained" color="primary" type="submit">
+                    Login
+                </Button>
             </div>
         </form>
     );
