99495e85a9dc5a21172f0d18d622e28377d04bdf
Tibsen <91956517+Tibsen@users.noreply.github.com>
2022-04-24 19:41:28 +0200

    Add files via upload

---
+++ Pim/src/pages/LoginPage.jsx
@@ -0,0 +1,54 @@
+/* import React, {useState} from 'react';
+import {TextField} from "@mui/material";
+import { Button } from '@mui/material';
+import "./LoginPage.css" */
+
+/* const LoginPage = () => {
+    const [credentials, setCrentials] = useState({
+        identifier:"",
+        password:""
+    });
+
+    const handleChange =({currentTarget}) => {
+        console.log(currentTarget)
+        const {value, name} = currentTarget
+        setCrentials({
+            ...credentials,
+            [name]: value
+        })
+    }
+
+    return (
+        <form>
+            <div className='connexion'>
+                <TextField
+                id="identifier"
+                label="Username"
+                type="text"
+                name="identifiant"
+                onChange={handleChange}
+                />
+            </div>
+            <div className='connexion'>
+                <TextField
+                id="password"
+                label="Password"
+                type="text"
+                name="password"
+                onChange={handleChange}
+                />
+            </div>
+            <div className='oublie'>
+                <a href=''>Identifiant oublié</a>
+            </div>
+            <div className='connexion'>
+                <Button variant="contained" color="primary" type="submit">
+                    Login
+                </Button>
+            </div>
+        </form>
+    );
+}
+
+export default LoginPage; */
+
